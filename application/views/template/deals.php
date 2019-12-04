8bb2e47cbf44986fe1efe53025acddc5b3e026ac
Antonin <anto.nowak67@gmail.com>
2019-12-04 13:48:16 +0100

    Deals

--- application/views/template/deals.php
+++ application/views/template/deals.php
@@ -1,12 +1,12 @@
 <div class="card mb-3" style="max-width: 540px;">
 	<div class="row no-gutters">
 		<div class="col-md-4">
-			<img src="user_guide/_images/90351-full.png" class="card-img" alt="Image Operateur">
+			<img src=" <?php echo $de->img; ?> " class="card-img" alt="Image Operateur">
 		</div>
 		<div class="col-md-8">
 			<div class="card-body">
 				<a class="card-title" href="details"><?php echo $de->nom; ?></a>
-				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				<p class="card-text"><?php echo $de->description; ?></p>
 				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 			</div>
 		</div>
