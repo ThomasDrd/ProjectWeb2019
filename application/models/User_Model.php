0ca83690f345ef154a6d54d6ae2f19698b3f8d1d
Antonin <anto.nowak67@gmail.com>
2019-12-05 15:58:06 +0100

    avancement

--- application/models/User_Model.php
+++ application/models/User_Model.php
@@ -9,4 +9,9 @@ class User_Model extends CI_Model
 		return  $this->db->query('SELECT * FROM users')->result();
 	}
 
+	public function userInfo($user)
+	{
+		return 	$this->db->query('SELECT * FROM users WHERE user_id = '.$user);
+	}
+
 }
