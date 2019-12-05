c16b8d810da35af6d201bd8ab4d0474228e5cacd
Antonin <anto.nowak67@gmail.com>
2019-12-05 11:19:26 +0100

    broutille

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -12,28 +12,28 @@
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	<a class="navbar-brand" href="index">Super Forfait Mobile</a>
+		<a class="navbar-brand" href="index">Super Forfait Mobile</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
 
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto">
-			<li class="nav-item active">
-				<a class="nav-link" href="login">Home <span class="sr-only">(current)</span></a>
-			</li>
+			<?php if(!isset($_SESSION['user'])){
+			 	echo ('<li class="nav-item active">
+						<a class="nav-link" href="login">Log In<span class="sr-only">(current)</span></a>
+					   </li>');
+			} ?>
+			
 			<li class="nav-item">
 				<a class="nav-link" href="#">Features</a>
 			</li>
 			<li class="nav-item">
 				<a class="nav-link" href="#">Pricing</a>
 			</li>
-			<li class="nav-item">
-				<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-			</li>
 			<?php if(isset($_SESSION['user'])){
-			 	echo ('<li class="nav-link">
-						Welcome '.$_SESSION['user'].'
+			 	echo ('<li class="nav-item">
+						<a class="nav-link" href="users" tabindex="-1" aria-disabled="true">Welcome '.$_SESSION['user'].'</a>
 					   </li>');
 			} ?>
 			<?php if(isset($_SESSION['user'])){
