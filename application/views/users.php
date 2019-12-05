bc372a2d99916e66b25a7e7d7e8866b7d0008719
Antonin <anto.nowak67@gmail.com>
2019-12-05 16:33:21 +0100

    some change

--- application/views/users.php
+++ application/views/users.php
@@ -6,8 +6,10 @@ include 'template/header.php'; ?>
 			<form action="edituser" method="post">
 				<fieldset>
 					<legend>Sco pa tu mana</legend>
-					<p><label for="name">Nom</label>	<input id="name" type="text" name="name" value="" /></p>
-					<p><label for="pwd">Mot de passe</label>	<input id="pwd" type="text" name="pwd" value="" /></p>
+					<p><label for="name">Pseudo</label><input id="name" type="text" name="name" value="" placeholder="  <?php print_r($user['0']->pseudo); ?> "/></p>
+					<p><label for="pwd">Mail</label><input id="mail" type="text" name="mail" value="" placeholder="  <?php print_r($user['0']->mail); ?> "/></p>
+					<p><label for="pwd">Mot de passe</label><input id="pwd" type="text" name="pwd" value=""/></p>
+					<p><label for="pwd">Mot de passe confirmation</label><input id="pwdconf" type="text" name="pwdconf" value=""/></p>
 					<p><input type="submit" name="search"/></p>
 				</fieldset> 
 			</form>
