601c65728e460f6755b566c094eb8fbd5af83ddb
Thomas <thomas.duranddut@gmail.com>
2020-01-25 00:12:46 +0100

    modifs

--- application/views/copyright.php
+++ application/views/copyright.php
@@ -1,5 +1,6 @@
 <?php
 include 'template/header.php';
+echo '<div class = "copyright">';
 echo "<h1>Les Supers Forfaits Mobiles – <br>Conditions Générales d’Utilisation</h1>";
 echo "<p>«Les Supers Forfaits Mobiles» est un site Internet/application web qui vous propose une méthode de retrouver un ensemble de deal de forfaits mobiles. <br> Pour bénéficier de ces services, il convient de prendre connaissance et d’adhérer aux «Conditions Générales d’Utilisation », ci-après définies. <br> L’expression « Conditions Générales d’Utilisation », signifie l’ensemble des stipulations prévues aux présentes.</p>";
 
@@ -47,5 +48,6 @@ echo "<h1>Laisser un commentaire</h1>";
 </div>
 
 </form>
+</div>
 <?php
 include 'template/footer.php';
