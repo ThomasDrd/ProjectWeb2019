dca992d8084076323cd88b3d0f4d3ee3fa154585
unknown <antonin.nowak@viacesi.fr>
2020-02-05 20:54:09 +0100

    'commentaire2'

--- application/views/moveUser.php
+++ application/views/moveUser.php
@@ -1,4 +1,6 @@
 <?php 
 include 'template/header.php';
-	echo '<img src="'.base_url('user_guide/_images/user.jpg').'"</img>';
+	echo '<img src="https://www.slate.fr/sites/default/files/styles/1060x523/public/hide_the_pain_harold_meme_andras_arato.jpg"</img>';
 include 'template/footer.php';
+
+// '.base_url('user_guide/_images/user.jpg').
